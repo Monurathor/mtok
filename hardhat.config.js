907227f2c8f4f3a757cbf48e23cf5d7b4394f49e
Monu Rathor <kuldeeprathor0101@gmail.com>
2023-06-21 13:07:00 +0530

    envoirment variables added

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   networks: {
     hardhat: {},
     alchemy: {
-      url: process.env.ALCHEMY_RPC_ENDPOINT,
-      accounts: [process.env.PRIVATE_KEY],
+      url: "https://eth-goerli.g.alchemy.com/v2/55Zz-IusEEHn_ivLQBrNh9Y0M3s_97Cwprocess.env.ALCHEMY_RPC_ENDPOINT",
+      accounts: "084f2f34b37fd0d3319ad9841f354887804c2abbd6497384c3e7b88148e4a4ad",
     },
   },
   solidity: {
